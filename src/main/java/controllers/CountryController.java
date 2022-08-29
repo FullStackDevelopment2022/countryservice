13d7d34e00f77f0c50d8fc766d21220f6341a363
sumayyayellow <sumayyayellow@gmail.com>
2022-08-29 15:25:13 -0500

    first commit

---
+++ src/main/java/controllers/CountryController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import services.CountryService;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    CountryService countryService;
+
+
+}
